f7821623af1d8adf024f7fe3840d23d2ff18df94
Jisu <jaejisu@gmail.com>
2017-11-15 00:52:58 -0800

    modify width

--- pages/header.php
+++ pages/header.php
@@ -48,3 +48,17 @@
 </div>
         <!-- </div> -->
     </nav>
+
+<!-- <button class="hamburger">&#9776;</button>
+<button class="cross">&#735;</button>
+</header>
+
+<div class="menu">
+  <ul>
+    <a href="#"><li>LINK ONE</li></a>
+    <a href="#"><li>LINK TWO</li></a>
+    <a href="#"><li>LINK THREE</li></a>
+    <a href="#"><li>LINK FOUR</li></a>
+    <a href="#"><li>LINK FIVE</li></a>
+  </ul>
+</div> -->
