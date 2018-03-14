905f8ce125dfc213f3a48645dc2a238aba3505b3
Jisu Lee <jaejisu@gmail.com>
2018-03-14 16:12:15 -0700

    add images

--- latale.php
+++ latale.php
@@ -93,7 +93,7 @@
         <div class="eighthrow-container">
         <div class="max-width pb-flexbox">
             <a href="disney.php" class="pb-tag"><div class="previous"><i class="fa fa-angle-left" aria-hidden="true"></i> previous</div></a>
-            <a href="war.php" class="pb-tag"><div class="back">back <i class="fa fa-angle-right" aria-hidden="true"></i></div></a>
+            <a href="war.php" class="pb-tag"><div class="back">back<i class="fa fa-angle-right" aria-hidden="true"></i></div></a>
         </div>
         </div>
 
