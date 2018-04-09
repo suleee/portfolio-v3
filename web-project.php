a3d7ac582ca20d780f8a8ac6fc15504833fa6506
Jisu Lee <jaejisu@gmail.com>
2018-04-09 10:24:55 -0700

    add links

--- web-project.php
+++ web-project.php
@@ -8,10 +8,6 @@
             </div>
         </div>
 
-            <!-- <div>
-                <h3></h3>
-                <div class="bar"></div>
-            </div> -->
             <div class="web-p-container">
  
 
@@ -38,7 +34,8 @@
                     </p>
                     <p class="lang"><span>language:</span> HTML5, CSS3, JavaScript and jQuery</p></div>
                     <div class="git-btn">
-                        <a href="https://github.com/suleee/fellow-teaser" class="git-link git-link-pp"> View in GitHub</a></div>
+                        <a href="http://felloweternalclash.com/" class="git-link git-link-pp"> Visit site</a>
+                        <a href="https://github.com/suleee/fellow-teaser" class="git-link git-link-pp">GitHub</a></div>
                 </div>
 
                 <div class="web-p-single-container">
@@ -47,14 +44,16 @@
                     </div>
                     <!-- <div class="pink-bar am-wp-bar"></div> -->
                     <div class="text"><p>This is a community project that is synthesized everything I have learned throughout the semester to best decide how to tackle the assigned project. It must incorporate all of the best practices I have learned about HTML, CSS, SASS, Javascript/jQuery, and PHP.
-                        <!-- Also learn how to use Git for version control and team
-                        collaboration. -->
 
-                    <!-- <p><span>language:</span> HTML, CSS, Sass, Javascript, jQuery, Wordpress, PHP</p> -->
                     </p>
                 </div>
-                    <div class="git-btn"><a href="https://github.com/suleee/catalyst-winter-2017" class="git-link git-link-b">View in GitHub</a></div>
+                    
+                    <div class="git-btn">
+                    <a href="http://catalystcommdev.org/" class="git-link git-link-b">Visit site</a>
+                    <a href="https://github.com/suleee/catalyst-winter-2017" class="git-link git-link-b">GitHub</a></div>
                 </div>
+
+
                 <div class="web-p-single-container">
                     <div class="web-title"><img src="img/ajs-logo-color.png" class="wp" alt="wordpress">
                     <h4>Mars Colony App | Angular2 JS</h4></div>
@@ -62,8 +61,11 @@
                     <div class="text"><p>Single web app which includes a home and registration page, an alien encounters and reports page and
                         a blog page. </p>
                     <p class="lang"><span>language:</span> Angular2 JS, CSS3/SASS and Typescript</p></div>
-                    <div class="git-btn"><a href="https://github.com/suleee/MarsColonyApp" class="git-link git-link-p">View in GitHub</a></div>
+                    <div class="git-btn">
+                    <a href="https://github.com/suleee/MarsColonyApp" class="git-link git-link-p">GitHub</a></div>
                 </div>
+
+
                 <div class="web-p-single-container">
                     <div class="web-title"><img src="img/wordpress-icon-color.png" class="wp" alt="wordpress">
                     <h4>Inhabitent | WordPress</h4></div>
@@ -71,8 +73,12 @@
                     <div class="text"><p>Custom theme with custom post types, a custom taxonomy, custom widgets, and a custom plugin.
                     </p>
                     <p class="lang"> <span>language:</span> HTML, CSS, Sass, Javascript, jQuery, Wordpress, PHP</p></div>
-                    <div class="git-btn"><a href="https://github.com/suleee/Inhabitent_project4" class="git-link git-link-b">View in GitHub</a></div>
+                    <div class="git-btn">
+                    <a href="http://inhabitant.akkarapaka.com/" class="git-link git-link-b">Visit site</a>
+                    <a href="https://github.com/suleee/Inhabitent_project4" class="git-link git-link-b">GitHub</a></div>
                 </div>
+
+
                 <div class="web-p-single-container">
                     <div class="web-title"><img src="img/nyt-t-logo.png" class="wp in" alt="wordpress">
                     <h4>INSTANEWS  | NY Times API</h4></div>
@@ -81,18 +87,14 @@
                         the user selected in the drop down select box.
                     </p>
                     <p class="lang"><span>language:</span> HTML5, CSS3, JavaScript, jQuery and AJAX/JSON API</p></div>
-                    <div class="git-btn"><a href="https://github.com/suleee/Instanews-App" class="git-link git-link-g">View in GitHub</a></div>
+                    
+                    <div class="git-btn">
+                    <a href="https://suleee.github.io/Instanews-App/" class="git-link git-link-g">Visit site</a>
+                    <a href="https://github.com/suleee/Instanews-App" class="git-link git-link-g">GitHub</a></div>
+                </div>
 
-                </div>
-                <div class="web-p-single-container">
-                    <div class="web-title"><img src="img/wordpress-icon-color.png" class="wp" alt="wordpress">
-                    <h4>GradusOne | WordPress</h4></div>
-                    <!-- <div class="pink-bar am-wp-bar"></div> -->
-                    <div class="text"><p>Custom theme with custom post types, a custom taxonomy, custom widgets, and a custom plugin.
-                    </p>
-                    <p class="lang"><span>language:</span> HTML, CSS, Sass, Javascript, jQuery, Wordpress, PHP</p></div>
-                    <div class=" git-btn"><a href="https://github.com/suleee/gradusone" class="git-link git-link-b">View in GitHub</a></div>
-                </div>
+
+
                  <div class="web-p-single-container">
                     <div class="web-title"><img src="img/resp-icon-color.png" class="wp" alt="responsiceweb">
                     <h4>Aloha | Responsive Web</h4></div>
@@ -100,8 +102,10 @@
                     <div class="text"><p> The Aloha Apparel Co. The website build with HTML, CSS components, and interactive JS. A responsive web design implemented using the mobile first approach. 
                     </p>
                     <p class="lang"><span>language:</span> HTML5, CSS3, JavaScript and jQuery</p></div>
-                    <div class="git-btn">
-                  <a href="https://github.com/suleee/Aloha-Responsive-Website" class="git-link git-link-pp"> View in GitHub</a></div>
+                  
+                  <div class="git-btn">
+                  <a href="https://suleee.github.io/Aloha-Responsive-Website/" class="git-link git-link-pp"> Visit site</a>
+                  <a href="https://github.com/suleee/Aloha-Responsive-Website" class="git-link git-link-pp"> GitHub</a></div>
                 </div>
 
             </div>
