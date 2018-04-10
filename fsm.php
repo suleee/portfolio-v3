176536d073e3e1cf6524657ce88aa7f46fc3b949
Jisu Lee <jaejisu@gmail.com>
2018-04-10 12:42:45 -0700

    change the link

--- fsm.php
+++ fsm.php
@@ -53,7 +53,7 @@
 
         <div class="fourthrow-container">
         <div class="max-width pb-flexbox">
-            <a href="adbuyer.php" class="pb-tag"><div class="previous"><i class="fa fa-angle-left" aria-hidden="true"></i> previous</div></a>  
+            <a href="fellow.php" class="pb-tag"><div class="previous"><i class="fa fa-angle-left" aria-hidden="true"></i> previous</div></a>  
             <a href="pocha.php" class="pb-tag"><div class="back">back <i class="fa fa-angle-right" aria-hidden="true"></i></div></a>
         </div>
         </div>
